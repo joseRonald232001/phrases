363922b482f6bac54747055b08ced86d8d5fc5f6
joseRonald232001 <phjronald@gmail.com>
2023-06-08 18:53:43 -0400

    inicio del proyecto

---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import phrases from "../phrases.json"
+const Cart = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Cart;
